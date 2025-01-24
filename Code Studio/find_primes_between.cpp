b6f3c46584e9c74124310614c99c86afdc7cefcd
Animesh Raj <animeshraj80707@gmail.com>
2025-01-24 22:12:24 +0530

    Update find_primes_between.cpp

--- Code Studio/find_primes_between.cpp
+++ Code Studio/find_primes_between.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 int main(){
     int num , num2 , k , count = 0;
-    cin>>num>>num2;
+    cin>>num>>num2; // enter two numbers between you want to print prime numbers.
     for(int i = num + 1 ; i < num2 ; i++ ){
         for ( k = 2 ; k < i ; k++ ){
             if ( i % k == 0 ){
