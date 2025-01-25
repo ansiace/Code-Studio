625edccc46bea74a6997d7fc044efe071b96ebf0
Animesh Raj <animeshraj80707@gmail.com>
2025-01-25 18:32:19 +0530

    Update find_max_in_array.cpp

--- Code Studio/find_max_in_array.cpp
+++ Code Studio/find_max_in_array.cpp
@@ -3,7 +3,7 @@ using namespace std;
 int main(){
     int array[] = { 0, 4, 5, 3, 8, 6, 1, 2 };
     int n = sizeof (array) / sizeof (int);
-    int max = array[0];
+    int max = array[0]; //taking one element from the given array to compare.
     for (int i = 0 ; i < n ; i++){
         if ( array[i] > max ){
             max = array[i];
