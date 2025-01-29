c2d8402cdd6562c9ab7d2840d915d97ae75f49e0
Animesh Raj <animeshraj80707@gmail.com>
2025-01-29 22:05:59 +0530

    Update CheckPowerofTwo.cpp

--- Code Studio/CheckPowerofTwo.cpp
+++ Code Studio/CheckPowerofTwo.cpp
@@ -3,7 +3,7 @@ using namespace std;
 int main(){
     int n;
     cin>>n;
-    if ( n && !(n & n-1)){
+    if ( n && !(n & n-1)){  // The result of ( n & n-1 ) is always zero.
         cout<<n<<" is a power of 2";
     }
     else{
