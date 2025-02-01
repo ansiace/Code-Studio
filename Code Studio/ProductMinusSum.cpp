1d87f84393ebf68d8e7dd451b0491d1d3abe90d6
Animesh Raj <animeshraj80707@gmail.com>
2025-02-01 22:02:31 +0530

    Update ProductMinusSum.cpp

--- Code Studio/ProductMinusSum.cpp
+++ Code Studio/ProductMinusSum.cpp
@@ -7,8 +7,8 @@ int main(){
     int r , sum = 0 , prod = 1 , ans ;
     while( n != 0 ){
         r = n % 10 ;
-        prod = prod * r ;
-        sum = sum + r ;
+        prod = prod * r ; // product
+        sum = sum + r ; // sum
         n = n / 10 ;
     }
     ans = prod - sum ;
